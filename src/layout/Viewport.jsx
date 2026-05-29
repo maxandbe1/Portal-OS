51716df3d6eec888a206bf679b3e32204233c4c7
maxandbe1 <maxandbe1@proton.me>
2026-05-29 16:14:07 -0400

    Update Viewport.jsx

--- src/layout/Viewport.jsx
+++ src/layout/Viewport.jsx
@@ -2,9 +2,15 @@ import { IdentityView } from "../modules/identity/IdentityView.jsx";
 import { PatternView } from "../modules/pattern/PatternView.jsx";
 import { MemoryView } from "../modules/memory/MemoryView.jsx";
 import { BeeSimView } from "../modules/beesim/BeeSimView.jsx";
+import { ModuleInspector } from "../ModuleInspector.jsx";
+
+
+
 
 export function Viewport({ current }) {
   switch (current) {
+   case "inspector": return <ModuleInspector />;
+
     case "identity": return <IdentityView />;
     case "pattern": return <PatternView />;
     case "memory": return <MemoryView />;
