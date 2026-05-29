249769720f21f6256b285e2fa2bfa90189f0f86f
maxandbe1 <maxandbe1@proton.me>
2026-05-29 14:43:33 -0400

    Update Viewport.jsx

--- src/layout/Viewport.jsx
+++ src/layout/Viewport.jsx
@@ -1,12 +1,16 @@
 import React from "react";
 import { IdentityView } from "../modules/identity/IdentityView.jsx";
+import { PatternView } from "../modules/pattern/PatternView.jsx";
+import { MemoryView } from "../modules/memory/MemoryView.jsx";
+import { BeeSimView } from "../modules/beesim/BeeSimView.jsx";
 
 export function Viewport({ current }) {
   return (
     <>
       {current === "identity" && <IdentityView />}
       {current === "pattern" && <PatternView />}
-
+      {current === "memory" && <MemoryView />}
+      {current === "beesim" && <BeeSimView />}
     </>
   );
 }
